ba96e12f8a03f498c468618dfac4c90930bc81b6
WindanceBoardshop <windance@windance.com>
2025-01-15 09:24:07 -0800

    fix libs

--- LSPD_Tokens_refresh.R
+++ LSPD_Tokens_refresh.R
@@ -1,3 +1,7 @@
+library(httr2)
+library(openssl)
+library(sodium)
+
 # authentication Variables
 
 LSPDAUTH <- list()
@@ -6,13 +10,16 @@ LSPDAUTH$LSPDClientID <- '0abc655ba630e8de82f6d083987f7ce9a4e6d84d5e4846a7c04819
 LSPDAUTH$LSPDClientSecretKey <- 'f259afa4db702a7aba731499d277a4049e65f95a53ad5cb1f4862fcbc81e4d97'
 LSPDAUTH$LSPDRefreshToken <- ''
 LSPDAUTH$AccessToken <- ''
-LSPDAUTH$AUTHCODE <- 'def502003b95253d40164991cfd50ad005545fdfbc46a64f53ab16e464796cfe55cfec325a4b8052bfbecdd77579b340cbf12d141eed8f95a36ce3b08410db5e98731b77ab127930cf8b7a2d81cea143f59b0319b2ebf068db187c877be1133df19af9d9d1e26f1f0fcbd3c9cae9858de18c1d6c7a562f7e7a5a799eb12d7eb4aa9d12cbfbbc273486e3386bbf372b9d23f0871aa6906af65a884ebb87f9a5ddebe046e098e78898d94a50e79b8ab3614624db10771827bd47203621ee1c9c35848fdc080801c568686b0b4560ea51af22dd555c523e7d5efa570a194a963a7d2901ff5ebaf059d3c1d1fb8983e5b214472f8a9de3f30b44408a48cb9395cc3e3b0daf7a5e99404a5e43867553a4a72bbc745fc12119bfb97c3f98f8eb213bebf26da2e8f590a863c97246dcfa8188cafc3a65f584c77ccc382c34665e133918005a45cd4b9b8ee02d77c9fc1297b63d32b4a6155ff040814ebf9be989d5f101fe8e254185c9b1467adb922a60e37bd335c9dded327e5030e6a449976824cc02dac654c4438a36a3fbceb820d1a931a28c40d2cd'
+LSPDAUTH$AUTHCODE <- ''
 
 # granting access in browse
 
 utils::browseURL(paste0('https://cloud.lightspeedapp.com/auth/oauth/authorize?response_type=code&client_id=', 
        LSPDAUTH$LSPDClientID,
        '&scope=employee:all'))
+
+# update AUTH code
+LSPDAUTH$AUTHCODE <- 'def5020036501d4305af0ac1c8d3af8e444012e56671fba4ecd4fd31c46778900072c484ae17c44ffa7fe86fe0652b87fbebdf6b8a854dc913c7b2193de8d92749052857e6f68e85333b090e1af420d83cdaed3bdda956327ababfc16b626a7ab53e02337f7352706e3edf633379a2367d1f4a0de305029da1770360187fcbd8aab7e1a239ae7cb5284f37db5de7843727a8a150ccdf8b2b33cfa09e2e6846d661df9b7790b47f5ae13a9e5e356d704f685bd053e5c300d665ebb6642fbadb62e088caa024342402cb62a87b403140b371d4f29ccaff99a3cbc79f99d71c30d70dc37f8f2981b2d57b65f5954f53d1a27ebabbe05122f9c2cc77a2b29eb8fe499cd5b1fe8322b1e83fc1cb1e1a899def40ae1a691b92e276b12d8814625ae10523ea243aba79d5489d8a7179ba5c92e1b31ec5bb0a7bc8053d6186152bb656096cc9b8d37005d9941120a99190ede068d1ce3f7710d5d289afbed3beb21d4db023f45510cf742da2bbaf80497a635902cbc25580fac35c26495f398722a8260fea2ffe7734b8c28611b8edc06de4766d5f30afcc'
 
 # getting tokens
 
@@ -52,9 +59,10 @@ LSPDAUTH$AccessToken <- response_content$access_token
 # Github information and variables. load these variables from the system environment
 
 # keep this for github to run
-# GH$REPO_OWNER <- Sys.getenv("GH_REPO_OWNER")
-# GH$REPO_NAME <- Sys.getenv("GH_REPO_NAME")
-# GH$PAT <- Sys.getenv("GH_PAT")
+#GH <- list()
+#GH$REPO_OWNER <- Sys.getenv("GH_REPO_OWNER")
+#GH$REPO_NAME <- Sys.getenv("GH_REPO_NAME")
+#GH$PAT <- Sys.getenv("GH_PAT")
 
 # Use this to run in R
 GH <- list()
@@ -92,13 +100,7 @@ GH$KEY_ID <- public_key_data$key_id
 
 #######################################################
 # convert to raw for sodium
-secret_raw <- charToRaw(secret)
-# decode public key
-pub_key_gh_dec <- base64enc::base64decode(pub_key_gh)
-# encrypt using the pub key
-secret_raw_encr <- sodium::simple_encrypt(secret_raw, pub_key_gh_dec)
-# base64 encode secret
-secret_raw_encr <- base64enc::base64encode(secret_raw_encr)
+
 # # # # # # # # #  #
 
 
